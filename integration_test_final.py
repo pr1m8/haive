5ba8553325d01b4778dc6ee385ad8f310e9268ba
pr1m8 <william.astley@algebraicwealth.com>
2025-07-31 14:05:46 -0400

    feat(trunk): complete pre-commit integration with trunk actions

---
+++ integration_test_final.py
@@ -0,0 +1,6 @@
+# Final integration test for trunk + pre-commit
+print("Testing trunk + pre-commit integration")
+
+def test_function():
+    """Test function for integration."""
+    return "integration working"
